ed4008b1069e2816d664281bd33b4e4928515d2e
Adam Coffman <acoffman@genome.wustl.edu>
2014-02-21 14:09:30 -0600

    remove match statements in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,28 @@
 DruggableGene::Application.routes.draw do
-  match 'drug_claims/:source_db_name/:name' => 'drug_claims#show',
+  get 'drug_claims/:source_db_name/:name' => 'drug_claims#show',
     constraints: { name: /[^\/]+/ }
-  match 'gene_claims/:source_db_name/:name' => 'gene_claims#show'
-  match 'gene_names' => 'genes#names'
-  match 'genes/:name' => 'genes#show', as: 'gene'
-  match 'interaction_claims/:id' => 'interaction_claims#show', as: 'interaction_claims'
-  match 'druggable_gene_categories/:name' => 'genes#druggable_gene_category', as: 'gene_by_category'
-  match 'druggable_gene_categories' => 'genes#druggable_gene_categories'
-  match 'drugs/:name' => 'drugs#show', as: 'drug',
+  get 'gene_claims/:source_db_name/:name' => 'gene_claims#show'
+  get 'gene_names' => 'genes#names'
+  get 'genes/:name' => 'genes#show', as: 'gene'
+  get 'interaction_claims/:id' => 'interaction_claims#show', as: 'interaction_claims'
+  get 'druggable_gene_categories/:name' => 'genes#druggable_gene_category', as: 'gene_by_category'
+  get 'druggable_gene_categories' => 'genes#druggable_gene_categories'
+  get 'drugs/:name' => 'drugs#show', as: 'drug',
     constraints: { name: /[^\/]+/ }
-  match 'sources/:source_db_name' => 'sources#show', as: 'source'
-  match 'sources' => 'sources#sources'
-  match 'drugs/:name/related' => 'drugs#related_drugs', as: 'related_drug',
+  get 'sources/:source_db_name' => 'sources#show', as: 'source'
+  get 'sources' => 'sources#sources'
+  get 'drugs/:name/related' => 'drugs#related_drugs', as: 'related_drug',
     constraints: { name: /[^\/]+/ }
-  match 'search_results' => 'search#search_results'
-  match 'cache/invalidate' => 'utilities#invalidate_cache'
+  get 'search_results' => 'search#search_results'
+  get 'cache/invalidate' => 'utilities#invalidate_cache'
   post  'download_table' => 'utilities#download_request_content'
   post  'api/v1/interactions' => 'services_v1#interactions'
   post  'api/v1/related_genes' => 'services_v1#related_genes'
   get   'api/v1/:action' => 'services_v1#:action'
-  match 'interaction_search_results' => 'interaction_claims#interaction_search_results'
-  match 'interactions_for_related_genes' => 'interaction_claims#interactions_for_related_genes'
-  match 'categories_search_results' => 'genes#categories_search_results'
-  match ':action' => 'static#:action'
+  match 'interaction_search_results' => 'interaction_claims#interaction_search_results', via: [:get, :post]
+  match 'interactions_for_related_genes' => 'interaction_claims#interactions_for_related_genes', via: [:get, :post]
+  match 'categories_search_results' => 'genes#categories_search_results', via: [:get, :post]
+  get ':action' => 'static#:action'
   root :to => 'static#search_interactions'
 
   # The priority is based upon order of creation:
