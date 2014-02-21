188d1dcf7365a40ff350986de75de8eb6f8f8d1c
Adam Coffman <acoffman@genome.wustl.edu>
2014-02-21 14:22:12 -0600

    add new secret token for dev mode

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 DruggableGene::Application.config.secret_token = '7db37e448130b0726d07366b6ee59dacd04dcd8ff6742e82b4101adda7c644a7dd01f9586aa0c1d901d04e30c8ef62dfe9c1d1f30bcd8be48fcbe04aa1eb2900'
+DruggableGene::Application.config.secret_key_base = '97f2e59140989d206efac3b02172ecb9d0dae40ec7714a2c59ab04c0a6cd7e14de67bd1393471a6f58c03c711ed99b746e69d4d3e737d8ccda11782f69a0b2c9'
