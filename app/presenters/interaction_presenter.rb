840ea2bbfe2d769bd21a3847c50638aa272dc149
Susanna Kiwala <ssiebert@genome.wustl.edu>
2017-08-14 11:36:40 -0500

    First draft of v2 API
    This includes endpoints for all genes, drugs, and interactions,
    as well as for one gene (by name), one drug (by name), and one
    interaction (by id)

--- app/presenters/interaction_presenter.rb
+++ app/presenters/interaction_presenter.rb
@@ -9,4 +9,15 @@ class InteractionPresenter < SimpleDelegator
   def display_name
   	"#{@interaction.gene.name} AND #{@interaction.drug.name}"
   end
+
+  def data
+    {
+      id: interaction.id,
+      gene_name: interaction.gene.name,
+      drug_name: interaction.drug.name,
+      publications: interaction.publications.map(&:pmid),
+      interaction_types: interaction.interaction_types.map(&:type),
+      sources: interaction.sources.map(&:source_db_name),
+    }
+  end
 end
